4a43e42b329f919764b8f1d95f7853e6a132de29
Rahul Mittal <rahul.mittal1499@gmail.com>
2020-07-23 00:54:33 +0530

    added Books

---
+++ src/main/java/com/cen4010/geektext/Books.java
@@ -0,0 +1,115 @@
+package com.cen4010.geektext;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Books {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer bookCode;
+	
+	private Integer authorNum;
+	
+	private String title;
+	
+	private String publisherCode;
+	
+	private Double price;
+	
+	private String genre;
+	
+	private Date yearPub;
+	
+	private Integer copiesSold;
+	
+	private String bookDescription;
+	
+	private Double averageRating;
+	
+	public Integer getBookCode() {
+		return bookCode;
+	}
+
+	public void setBookCode(Integer bookCode) {
+		this.bookCode = bookCode;
+	}
+
+	public Integer getAuthorNum() {
+		return authorNum;
+	}
+
+	public void setAuthorNum(Integer authorNum) {
+		this.authorNum = authorNum;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPublisherCode() {
+		return publisherCode;
+	}
+
+	public void setPublisherCode(String publisherCode) {
+		this.publisherCode = publisherCode;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public Date getYearPub() {
+		return yearPub;
+	}
+
+	public void setYearPub(Date yearPub) {
+		this.yearPub = yearPub;
+	}
+
+	public Integer getCopiesSold() {
+		return copiesSold;
+	}
+
+	public void setCopiesSold(Integer copiesSold) {
+		this.copiesSold = copiesSold;
+	}
+
+	public String getBookDescription() {
+		return bookDescription;
+	}
+
+	public void setBookDescription(String bookDescription) {
+		this.bookDescription = bookDescription;
+	}
+
+	public Double getAverageRating() {
+		return averageRating;
+	}
+
+	public void setAverageRating(Double averageRating) {
+		this.averageRating = averageRating;
+	}
+
+
+}
---
+++ src/main/java/com/cen4010/geektext/Main.java
@@ -0,0 +1,21 @@
+package com.cen4010.geektext;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller	// This means that this class is a Controller
+@RequestMapping(path="/demo") 
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
