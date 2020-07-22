07b8c80fc81687232e039dc7a446319abc8f88e0
Rahul Mittal <rahul.mittal1499@gmail.com>
2020-07-23 00:43:12 +0530

    upload Springer project

---
+++ src/main/java/com/cen4010/geektext/GeektextApplication.java
@@ -0,0 +1,13 @@
+package com.cen4010.geektext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeektextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeektextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cen4010/geektext/GeektextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cen4010.geektext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeektextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
